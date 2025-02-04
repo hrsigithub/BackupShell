5352dfc8c9676a966d3d4780f0ce56faa074badd
Hiroshi Nakai <hrsi.ad@gmail.com>
2025-02-04 16:19:56 +0900

    修正

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
-/usr/bin/rsync -avh --progress --delete --inplace --partial /Volumes/tp-share/03_music /Volumes/Time\ Capsle/TPShereBackup/
-/usr/bin/rsync -avh --progress --delete --inplace --partial /Volumes/tp-share/02-soft /Volumes/Time\ Capsle/TPShereBackup/
-/usr/bin/rsync -avh --progress --delete --inplace --partial /Volumes/tp-share/99_エミュレータ /Volumes/Time\ Capsle/TPShereBackup/
-/usr/bin/rsync -avh --progress --delete --inplace --partial /Volumes/tp-share/01_動画 /Volumes/Time\ Capsle/TPShereBackup/
-/usr/bin/rsync -avh --progress --delete --inplace --partial /Volumes/tp-share/04_fromBray /Volumes/Time\ Capsle/TPShereBackup/
+/usr/bin/rsync -avh --ignore-errors --progress --delete --inplace --partial /Volumes/tp-share/03_music /Volumes/Time\ Capsle/TPShereBackup/
+/usr/bin/rsync -avh --ignore-errors --progress --delete --inplace --partial /Volumes/tp-share/02-soft /Volumes/Time\ Capsle/TPShereBackup/
+/usr/bin/rsync -avh --ignore-errors --progress --delete --inplace --partial /Volumes/tp-share/99_エミュレータ /Volumes/Time\ Capsle/TPShereBackup/
+/usr/bin/rsync -avh --ignore-errors --progress --delete --inplace --partial /Volumes/tp-share/01_動画 /Volumes/Time\ Capsle/TPShereBackup/
+/usr/bin/rsync -avh --ignore-errors --progress --delete --inplace --partial /Volumes/tp-share/04_fromBray /Volumes/Time\ Capsle/TPShereBackup/
